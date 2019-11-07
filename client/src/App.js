673bb17c12fca108b6ea84e4a761669929c49c5d
neil <nvolungis@gmail.com>
2019-11-07 14:49:07 -0500

    production build and receive web sockets

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import useWebSocket from 'react-use-websocket';
 import './App.css';
 
 function App() {
+  const socketUrl = 'ws://localhost:4000';
+  const [, lastMessage, readyState] = useWebSocket(socketUrl);
+  const [cardId, setCardId] = useState();
+
+  useEffect(() => {
+    if (lastMessage !== null) {
+      setCardId(lastMessage.data);
+    }
+  }, [ lastMessage, setCardId ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>what up</h1>
+      {cardId && <span>cardId: {cardId}</span>}
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,13 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 4000
+const path = require('path');
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.post('/card', (req, res) => {
-  console.log('got a post')
-  console.log(req);
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
+const port = 4000;
+
+let websockets = [];
+
+wss.on('connection', (ws) => websockets.push(ws));
+
+app.use(express.static(path.join(__dirname, '../', 'client', 'build')));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../', 'client', 'build', 'index.html')));
+app.get('/card', (req, res) => {
+  const cardId = req.query["card"];
+  websockets.forEach(ws => ws.send(cardId));
   res.send('ok');
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+server.listen(port, () => console.log(`Example app listening on port ${port}!`))
