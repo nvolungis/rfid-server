1c02f258f1f0c54d299f0fe43b12956fd126c353
neil <nvolungis@gmail.com>
2019-11-07 16:14:08 -0500

    add wistia embed and map hashed ids

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import useWebSocket from 'react-use-websocket';
+import WistiaEmbed from './WistiaEmbed';
 import './App.css';
 
 function App() {
-  const socketUrl = 'ws://localhost:4000';
+  const socketUrl = 'ws://10.0.0.34:4000';
   const [, lastMessage, readyState] = useWebSocket(socketUrl);
   const [cardId, setCardId] = useState();
+  const [hashedId, setHashedId] = useState();
 
-  console.log(readyState);
-  console.log(lastMessage);
+  const cardMap = {
+    '701991537684': 'a3q7357e6f',
+    '7316510699229': 'j6gjf7n9nm',
+    '2718322021101': 'tbhm1r1jw0',
+    '1531831089933': '789q9wju99',
+    '419125099235': '86ak76l1fr',
+  }
+
+  useEffect(() => {
+    setHashedId(cardMap[cardId]);
+  }, [cardId]);
 
   useEffect(() => {
     if (lastMessage !== null) {
@@ -19,7 +30,15 @@ function App() {
   return (
     <div className="App">
       <h1>what up</h1>
+      <button onClick={() => setHashedId('a3q7357e6f')}>
+        change
+      </button>
       {cardId && <span>cardId: {cardId}</span>}
+      {hashedId ? (
+        <WistiaEmbed hashedId={hashedId} />
+      ) : (
+        <div>pick a vid!</div>
+      )}
     </div>
   );
 }
---
+++ client/src/WistiaEmbed.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useRef } from 'react';
+import useScript from './useScript';
+
+const WistiaEmbed = ({ hashedId }) => {
+  const url = 'https://fast.wistia.com/assets/external/E-v1.js'
+  const [loaded, error] = useScript(url);
+  const swatchRef = useRef();
+  const handle = useRef();
+
+  useEffect(() => {
+    if (handle.current) {
+      handle.current.replaceWith(hashedId);
+    } else {
+      window._wq = window._wq || [];
+      window._wq.push({
+        id: hashedId,
+        onReady: h => handle.current = h,
+      });
+    }
+  }, [hashedId]);
+
+  return (
+    <div
+      className="wistia_responsive_padding"
+      style={{
+        padding:'56.25% 0 0 0',
+        position: 'relative',
+      }}
+    >
+      <div
+        className="wistia_responsive_wrapper"
+        style={{
+          height:'100%',
+          left:0,
+          position: 'absolute',
+          top:0,
+          width: '100%',
+        }}
+      >
+        <div
+          className={`wistia_embed wistia_async_${hashedId} videoFoam=true autoPlay=true`}
+          style={{
+            height: '100%',
+            position: 'relative',
+            width: '100%',
+          }}
+        >
+          <div
+            ref={swatchRef}
+            className="wistia_swatch"
+            style={{
+              height:'100%',
+              left:0,
+              opacity:0,
+              overflow: 'hidden',
+              position:'absolute',
+              top:0,
+              transition:'opacity 200ms',
+              width: '100%',
+            }}
+          >
+            <img
+              src={`https://fast.wistia.com/embed/medias/${hashedId}/swatch`}
+              style={{
+                filter:'blur(5px)',
+                height:'100%',
+                objectFit:'contain',
+                width:'100%',
+              }}
+              onLoad={() => swatchRef.current.style.opacity=1 } />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
+};
+
+export default WistiaEmbed;
+
