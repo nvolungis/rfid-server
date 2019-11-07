5352029dca1d00556f27a14ec6d3d76f0c5115fd
neil <nvolungis@gmail.com>
2019-11-07 13:44:00 -0500

    break out client and server directories. add a post route for testing from rpi

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.post('/card', (req, res) => {
+  console.log('got a post')
+  console.log(req);
+
+  res.send('ok');
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
